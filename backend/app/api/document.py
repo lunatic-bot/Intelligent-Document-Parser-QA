77ed54c694dc61990e22a65b8ea1627b62526664
himanshi srivastava <himanshi0402@gmail.com>
2025-04-25 21:12:01 +0530

    udated

--- backend/app/api/document.py
+++ backend/app/api/document.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app import schemes, models, crud, dependencies
-
+#fvgbnh
 router = APIRouter(prefix="/documents", tags=["documents"])
